6672368dca99d7a2c037f3ce3aa57c6dfb988e2f
Kelly Bristol <bristolkr@gmail.com>
2015-05-28 15:57:41 -0400

    final

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,20 +1,3 @@
-# Create a "pig latin" translator, using a test-driven approach. 
-# In the past I've given you the tests to run, but this time you'll create your own from scratch.
-
-# Here is one approach to creating an API for your translator:
-
-# PigLatin.translate "hello" # => "ellohay"
-# I'll leave that decision up to you, but I do want you to include a full test suite that uses all the phrases I give below. 
-# They start from simple words and move on to phrases with capitalization and punctuation that should be preserved. 
-# You might need to use some Regular Expressions to get that done.
-
-# Remember to start simple, write a test for the first word, "human" and get it working, upping the complexity and refactoring your code as you go. 
-# Use past assignments for ideas on how to structure your project.
-
-# Bonus: Extend ruby's string class to be able to pigify a string:
-#   "Hello".pigify # => "Ellohay"
-
-
 class PigLatin
 
   def self.translate(phrase)
